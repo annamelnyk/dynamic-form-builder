b17baad185485f7358243f8da787d205ab3c49c4
Anna <blairw73@gmail.com>
2025-06-25 22:46:29 +1000

    add files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonToggleModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'dynamic-form-builder';
+  protected title = 'Dynamic Form Builder';
 }
