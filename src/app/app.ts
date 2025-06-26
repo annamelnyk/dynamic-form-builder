d859e1a4597dc74a4d7658b793eedc932bfefebb
Anna <blairw73@gmail.com>
2025-06-26 23:58:47 +1000

    Styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 import { Header } from './components/header/header';
 import { BuildForm } from './components/build-form/build-form';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, BuildForm],
+  imports: [RouterOutlet, Header, BuildForm, MatGridListModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
