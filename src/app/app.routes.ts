993e4d761130c74427565d723b975b2ce4a76888
Anna <blairw73@gmail.com>
2025-06-27 23:12:19 +1000

    Refactor build form component, update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BuildForm } from './components/build-form/build-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: BuildForm,
+  },
+];
--- src/app/components/build-form/build-form.ts
+++ src/app/components/build-form/build-form.ts
@@ -1,12 +1,22 @@
 import { Component, inject } from '@angular/core';
-import { ControlTool } from '../control-tool/control-tool';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { FormBuilderI, FormBuilderService } from '../../services/form-builder';
-
+import { ControlTool } from '../control-tool/control-tool';
 @Component({
   selector: 'app-build-form',
-  imports: [ControlTool, MatGridListModule],
+  imports: [
+    ControlTool,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './build-form.html',
   styleUrl: './build-form.scss',
 })
