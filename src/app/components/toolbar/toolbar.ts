f189dd2c854cfeb423b935d21e051feb058e0ebb
Anna <blairw73@gmail.com>
2025-06-28 23:13:19 +1000

    Styles update

--- src/app/components/toolbar/toolbar.ts
+++ src/app/components/toolbar/toolbar.ts
@@ -1,12 +1,13 @@
 import { Component, input, output } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { ControlTool } from '../control-tool/control-tool';
 import { FormBuilderI } from '../../services/form-builder';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [ControlTool, MatCardModule],
+  imports: [ControlTool, MatCardModule, MatGridListModule],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.scss',
 })
