d9b3ec8fb5d02d95a8b326c0f6193680658501bb
Anna <blairw73@gmail.com>
2025-07-22 22:38:16 +1000

    Add Prettier for code formatting

--- src/app/components/editable-form-field/editable-form-field.ts
+++ src/app/components/editable-form-field/editable-form-field.ts
@@ -1,4 +1,4 @@
-import { NgComponentOutlet, TitleCasePipe } from '@angular/common';
+import { NgComponentOutlet, TitleCasePipe } from '@angular/common'
 import {
   Component,
   computed,
@@ -8,13 +8,13 @@ import {
   OnInit,
   output,
   signal,
-} from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FieldType, FormFieldDefinitionValue } from '@model/form-fields';
-import { FormBuilderService } from '@services/form-builder/form-builder';
+} from '@angular/core'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatIcon, MatIconModule } from '@angular/material/icon'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { FieldType, FormFieldDefinitionValue } from '@model/form-fields'
+import { FormBuilderService } from '@services/form-builder/form-builder'
 
 @Component({
   selector: 'app-editable-form-field',
@@ -30,19 +30,19 @@ import { FormBuilderService } from '@services/form-builder/form-builder';
   styleUrl: './editable-form-field.scss',
 })
 export class EditableFormField implements OnInit {
-  formBuilderService = inject(FormBuilderService);
+  formBuilderService = inject(FormBuilderService)
 
-  form = input.required<FormGroup>();
-  formField = input.required<FormFieldDefinitionValue>();
-  isFirst = input<boolean>(false);
-  isLast = input<boolean>(false);
-  onMoveUp = output<Event>();
-  onMoveDown = output<Event>();
+  form = input.required<FormGroup>()
+  formField = input.required<FormFieldDefinitionValue>()
+  isFirst = input<boolean>(false)
+  isLast = input<boolean>(false)
+  onMoveUp = output<Event>()
+  onMoveDown = output<Event>()
 
-  FieldType = FieldType;
+  FieldType = FieldType
 
   get id(): string {
-    return this.formField().id;
+    return this.formField().id
   }
 
   ngOnInit(): void {}
@@ -50,19 +50,19 @@ export class EditableFormField implements OnInit {
   onTitleChange(e: Event) {}
 
   onClickMoveUp(e: Event) {
-    this.onMoveUp.emit(e);
+    this.onMoveUp.emit(e)
   }
 
   onClickMoveDown(e: Event) {
-    this.onMoveDown.emit(e);
+    this.onMoveDown.emit(e)
   }
 
   onRemoveField(e: Event) {
-    e.stopPropagation();
-    this.formBuilderService.removeFormFieldDefinition(this.formField().id);
+    e.stopPropagation()
+    this.formBuilderService.removeFormFieldDefinition(this.formField().id)
   }
 
   onChangeRequired() {
-    this.formBuilderService.toggleRequiredField(this.formField().id);
+    this.formBuilderService.toggleRequiredField(this.formField().id)
   }
 }
