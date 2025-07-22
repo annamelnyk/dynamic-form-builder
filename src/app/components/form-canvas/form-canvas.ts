e28c63a729515551b6b53146829c484b8f65103f
Anna <blairw73@gmail.com>
2025-07-22 22:52:03 +1000

    Add linting support with angular-eslint and eslint

--- src/app/components/form-canvas/form-canvas.ts
+++ src/app/components/form-canvas/form-canvas.ts
@@ -1,52 +1,45 @@
-import { Component, computed, inject } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
-import { MatCardModule } from '@angular/material/card';
+import { Component, computed, inject } from '@angular/core'
+import { FormGroup, ReactiveFormsModule } from '@angular/forms'
+import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop'
+import { MatCardModule } from '@angular/material/card'
 
-import { EditableFormField } from '@components/editable-form-field/editable-form-field';
-import { FieldTypesService } from '@services/field-types/field-types';
-import { FormBuilderService } from '@services/form-builder/form-builder';
-import { FormFieldControl } from '@services/form-field-control/form-field-control';
+import { EditableFormField } from '@components/editable-form-field/editable-form-field'
+import { FieldTypesService } from '@services/field-types/field-types'
+import { FormBuilderService } from '@services/form-builder/form-builder'
+import { FormFieldControl } from '@services/form-field-control/form-field-control'
 
 @Component({
   selector: 'app-form-canvas',
-  imports: [
-    MatCardModule,
-    EditableFormField,
-    DragDropModule,
-    ReactiveFormsModule,
-  ],
+  imports: [MatCardModule, EditableFormField, DragDropModule, ReactiveFormsModule],
   templateUrl: './form-canvas.html',
   styleUrl: './form-canvas.scss',
 })
 export class FormCanvas {
-  formBuilderService = inject(FormBuilderService);
-  fieldTypesService = inject(FieldTypesService);
-  formFieldControlService = inject(FormFieldControl);
+  formBuilderService = inject(FormBuilderService)
+  fieldTypesService = inject(FieldTypesService)
+  formFieldControlService = inject(FormFieldControl)
 
   form = computed<FormGroup>(() =>
-    this.formFieldControlService.toFromGroup(
-      this.formBuilderService.formFieldsList()
-    )
-  );
+    this.formFieldControlService.toFromGroup(this.formBuilderService.formFieldsList()),
+  )
 
-  payload = '';
+  payload = ''
 
   onFieldTypeDropped(event: CdkDragDrop<any[]>) {
-    console.log('dropped', event);
+    console.log('dropped', event)
 
     this.formBuilderService.addFormFieldToFromFieldDefinitionsList(
       event.item.data,
-      event.currentIndex
-    );
+      event.currentIndex,
+    )
   }
 
   moveFieldPosition(fromIndex: number, toIndex: number) {
-    console.log('clicked!');
-    this.formBuilderService.moveFromFieldDefinitionPosition(fromIndex, toIndex);
+    console.log('clicked!')
+    this.formBuilderService.moveFromFieldDefinitionPosition(fromIndex, toIndex)
   }
 
   onSubmit() {
-    this.payload = JSON.stringify(this.form().getRawValue());
+    this.payload = JSON.stringify(this.form().getRawValue())
   }
 }
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,13 +1,13 @@
-import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule, FormControl } from '@angular/forms';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, inject } from '@angular/core'
+import { ReactiveFormsModule, FormControl } from '@angular/forms'
+import { MatButtonToggleModule } from '@angular/material/button-toggle'
+import { MatIconModule } from '@angular/material/icon'
 
-import { AppTheme, Theme } from '@services/theme';
-import { MatToolbar } from '@angular/material/toolbar';
-import { OnWindowScroll } from '@directives/on-window-scroll';
-import { Router, RouterLink } from '@angular/router';
-import { NgTemplateOutlet } from '@angular/common';
+import { AppTheme, Theme } from '@services/theme'
+import { MatToolbar } from '@angular/material/toolbar'
+import { OnWindowScroll } from '@directives/on-window-scroll'
+import { Router, RouterLink } from '@angular/router'
+import { NgTemplateOutlet } from '@angular/common'
 
 @Component({
   selector: 'app-header',
@@ -24,12 +24,12 @@ import { NgTemplateOutlet } from '@angular/common';
   styleUrl: './header.scss',
 })
 export class Header {
-  themeService = inject(Theme);
-  router = inject(Router);
+  themeService = inject(Theme)
+  router = inject(Router)
 
-  theme = new FormControl<AppTheme>(this.themeService.selectedTheme());
+  theme = new FormControl<AppTheme>(this.themeService.selectedTheme())
 
   setSelectedTheme(theme: FormControl) {
-    this.themeService.setTheme(theme.value);
+    this.themeService.setTheme(theme.value)
   }
 }
